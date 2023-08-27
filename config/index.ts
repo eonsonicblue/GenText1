0684e96442a1bdecb8843c87668a609c04ba4a1b
eonsonicblue <krab483@gmail.com>
2023-08-27 03:11:04 -0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
-export const API_URL = ''
+export const APP_ID = '2f706c5b-719d-41d2-a2e8-9de573f0e8bc'
+export const API_KEY = '<Web API Key From Dify>'export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
   description: 'App description',
